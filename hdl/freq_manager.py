bafef31624a2e63d5614b6c208b51c16c291b2b9
Enrique Mendez <eqm@mit.edu>
2016-07-17 14:54:50 -0400

    Complete fsm.

--- hdl/freq_manager.py
+++ hdl/freq_manager.py
@@ -34,6 +34,7 @@ def freq_manager(
 	bin_count = Signal(intbv(0,min=0,max=10**N))
 	hex_count = Signal(intbv(0)[4*N:])
 	dig_incr  = Signal(intbv(0,min=0,max=N))
+
 	hex_counter_instance = hex_counter(
 			clk=clk, add=add, sub=sub,
 			bin_count=bin_count,
@@ -49,6 +50,15 @@ def freq_manager(
 		a value we subtract for allowing for convergence'''
 		dig_incr.next = fstep_rambus.dout - dig_incr_offset
 
+	@always_comb
+	def ram_read_signals():
+		freq_rambus.raddr.next 	= sched_addr
+		fstep_rambus.raddr.next	= sched_addr
+		tstep_rambus.raddr.next	= sched_addr
+		holdt_rambus.raddr.next	= sched_addr
+
+	sched = enum('START','INCREMENTING','HOLDING','FINISHED')
+	hold_counter = Signal(intbv(0)[32:])
 	@always_seq(clk.posedge,reset)
 	def schedule_stepper():
 		if state == sched.START:
@@ -56,19 +66,35 @@ def freq_manager(
 			dig_incr_offset.next	= 0
 			add.next            	= 0
 			sub.next            	= 0
+			hold_counter.next   	= 0
 			if trigger == 1:
 				state.next = sched.INCREMENTING
 		elif state == sched.INCREMENTING:
-
-		elif state == sched.HOLDING:
-
-
-			if sched_addr == sched_length:
-				state.next = sched.FINISHED
+			if   bin_count < freq_rambus.dout:
+				add.next	= 1
+				sub.next	= 0
+				if sub == 1:
+					dig_incr_offset.next = dig_incr_offset + 1
+			elif bin_count > freq_rambus.dout:
+				add.next	= 0
+				sub.next	= 1
+				if add == 1:
+					dig_incr_offset.next = dig_incr_offset + 1
 			else:
-				state.next = sched.INCREMENTING
+				add.next  	= 0
+				sub.next  	= 0
+				state.next	= sched.HOLDING
+		elif state == sched.HOLDING:
+			hold_counter.next = hold_counter + 1
+			
+			if hold_counter == holdt_rambus.dout:
+				sched_addr.next = sched_addr + 1
+				if sched_addr == sched_length - 1:
+					state.next = sched.FINISHED
+				else:
+					state.next = sched.INCREMENTING
 		elif state == sched.FINISHED:
-
+			hold_counter.next = 0
 			state.next = sched.FINISHED
 
-	return hex_counter_instance,digit_to_increment,schedule_stepper
+	return hex_counter_instance,digit_to_increment,ram_read_signals,schedule_stepper
